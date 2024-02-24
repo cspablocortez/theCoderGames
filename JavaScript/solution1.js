11a0f79cf01aed06e0ee4f8c205f1b0a279d30a9
Pablo Cortez <cspablocortez@gmail.com>
2024-02-24 18:14:20 -0500

    Refactor

--- JavaScript/solution1.js
+++
@@ -1,3 +0,0 @@
-let circumference = 22
-let radius = circumference / (2 * 3.14)
-console.log(radius)
--- JavaScript/solution3.js
+++
@@ -1,8 +0,0 @@
-let list = [1, 2, 3, 4, 5]
-let sum = 0
-
-for (let i = 0; i < list.length; i++) {
-    sum = sum + list[i]
-}
-
-console.log(sum / list.length)
