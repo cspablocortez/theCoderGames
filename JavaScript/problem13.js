8d8206e711775e5c2e2d2f643859ef97ea4a0283
Pablo Cortez <cspablocortez@gmail.com>
2024-02-27 21:13:58 -0500

    Create problem13.js

---
+++ JavaScript/problem13.js
@@ -0,0 +1,18 @@
+const arr = [1, 3, 3, 5, 7, 2, 6, 71, 16]
+
+function repeatedNumber(arr, number) {
+    let nextTo = false
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        console.log(`Current: ${arr[i]} -- Next: ${arr[i + 1]}`)
+
+        if (arr[i] === arr[i - 1]) {
+            nextTo = true
+            break
+        } 
+    }
+
+    return nextTo
+}
+
+console.log(repeatedNumber(arr, 3))  
